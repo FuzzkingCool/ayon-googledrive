475a8347d864be30a10fbeaa8f4712ed28d260d9
Aleks Berland <aleksberland@gmail.com>
2026-04-13 15:58:10 -0400

    Update version to 0.4.8 in package.py, pyproject.toml, and version.py to reflect the latest release. Allow project to override addon version.

--- client/ayon_googledrive/version.py
+++ client/ayon_googledrive/version.py
@@ -1,3 +1,3 @@
 # -*- coding: utf-8 -*-
 """Package declaring AYON addon 'googledrive' version."""
-__version__ = "0.4.7"
+__version__ = "0.4.8"
--- package.py
+++ package.py
@@ -1,6 +1,6 @@
 name = "googledrive"
 title = "GoogleDrive"
-version = "0.4.7"
+version = "0.4.8"
 client_dir = "ayon_googledrive"
 
 ayon_server_version = ">=1.1.2"
@@ -14,3 +14,5 @@ ayon_compatible_addons = {}
 # Mapping of addon name to version requirements
 ayon_optional_addons = {}
 
+# Allow project to override addon version
+project_can_override_addon_version = True
